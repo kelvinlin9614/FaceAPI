f3b72b96dd780c480430cc86fd2b147704962937
kevinlin9614 <kelvinlin9614@gmail.com>
2021-11-08 20:25:46 -0800

    Update(Ver 1.2) - Updated By SerlyT
    1. will return an emotion type based on the most reoccurring emotion
    2. add some helper function
    3. delete some useless code

--- FaceReader/FaceReader/EmotionReader.cs
+++ FaceReader/FaceReader/EmotionReader.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -29,83 +30,189 @@ namespace FaceReader
             return new FaceClient(new ApiKeyServiceClientCredentials(key)) { Endpoint = endpoint };
         }
 
+        //get the most occuring element 
+        private int findMostOccuringElement(int[] array, Hashtable hs)
+        {
+            int mostCommon = array[0];
+            int occurance = 0;
+            foreach (int num in array)
+            {
+                if (!hs.ContainsKey(num))
+                {
+                    hs.Add(num, 1);
+                }
+                else
+                {
+                    int tempOccurance = (int)hs[num];
+                    tempOccurance++;
+                    hs.Remove(num);
+                    hs.Add(num, tempOccurance);
+                    if (occurance < tempOccurance)
+                    {
+                        occurance = tempOccurance;
+                        mostCommon = num;
+                    }
+                }
+            }
+            return mostCommon;
+        }
+
+        //get the emotion of all the image of the video
+        private string GetHeaviestEmotion(IList<DetectedFace> imageList)
+        {
+            if(imageList.Count() != 0)
+            {
+                return imageList.FirstOrDefault().FaceAttributes.Emotion.ToRankedList().FirstOrDefault().Key;
+            }
+            else
+            {
+                return "null";
+            }
+        }
+
+
+        //get integer value for the given emotions 
+        private int GetReplyText(string emotion)
+        {
+            if (emotion == "Surprise" || emotion == "Happiness")
+            {
+                return 1;
+            }
+            else if (emotion == "Anger" || emotion == "Disgust")
+            {
+                return 2;
+            }
+            else if (emotion == "Fear")
+            {
+                return 3;
+            }
+            else if (emotion == "Sadness" || emotion == "Contempt")
+            {
+                return 4;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        //get the mood type 
+        private string moodType(int emotionLevel)
+        {
+            if (emotionLevel == 1)
+            {
+                return "Confident";
+            }
+            else if (emotionLevel == 2)
+            {
+                return "Angry";
+            }
+            else if (emotionLevel == 3)
+            {
+                return "Nervous";
+            }
+            else if (emotionLevel == 4)
+            {
+                return "Sad";
+            }
+            else
+            {
+                return "Neutral";
+            }
+        }
+
+
         public async Task DetectFaceExtract(Stream[] imageStream)
         {
-            int anger, contempt, disgust, fear, happiness, neutral, sadness, surprise;
-            anger = contempt = disgust = fear = happiness = neutral = sadness = surprise = 0;
+            //int anger, contempt, disgust, fear, happiness, neutral, sadness, surprise;
+            //anger = contempt = disgust = fear = happiness = neutral = sadness = surprise = 0;
             Console.WriteLine("========DETECT FACES========");
             Console.WriteLine();
-            
+
+            //save the moods in the array
+            int[] imageMoods = new int[imageStream.Length];
+
             for (int i = 0; i < imageStream.Length; i++)
             {
                 IList<DetectedFace> detectedFaces;
                 // Detect faces with all attributes from image stream.
                 detectedFaces = await this.client.Face.DetectWithStreamAsync(imageStream[i],
-                    returnFaceAttributes: new List<FaceAttributeType> { FaceAttributeType.Accessories, FaceAttributeType.Age,
-                    FaceAttributeType.Blur, FaceAttributeType.Emotion, FaceAttributeType.Exposure, FaceAttributeType.FacialHair,
-                    FaceAttributeType.Gender, FaceAttributeType.Glasses, FaceAttributeType.Hair, FaceAttributeType.HeadPose,
-                    FaceAttributeType.Makeup, FaceAttributeType.Noise, FaceAttributeType.Occlusion, FaceAttributeType.Smile },
+                    returnFaceAttributes: new List<FaceAttributeType> { FaceAttributeType.Emotion },
                     // We specify detection model 1 because we are retrieving attributes.
                     detectionModel: DetectionModel.Detection01,
                     recognitionModel: this.RECOGNITION_MODEL4);
                 Console.WriteLine($"{detectedFaces.Count} face(s) detected from {i}.jpg.");
-
-                // Parse and print all attributes of each detected face.
-                foreach (var face in detectedFaces)
-                {
-                    Console.WriteLine($"Face attributes for {i}.jpg:");
-                    // Get bounding box of the faces
-                    Console.WriteLine($"Rectangle(Left/Top/Width/Height) : " +
-                        $"{face.FaceRectangle.Left} {face.FaceRectangle.Top} {face.FaceRectangle.Width} {face.FaceRectangle.Height}");
-                    // Get emotion on the face
-                    string emotionType = string.Empty;
-                    double emotionValue = 0.0;
-                    Emotion emotion = face.FaceAttributes.Emotion;
-                    if (emotion.Anger > emotionValue) { emotionValue = emotion.Anger; emotionType = "Anger"; }
-                    if (emotion.Contempt > emotionValue) { emotionValue = emotion.Contempt; emotionType = "Contempt"; }
-                    if (emotion.Disgust > emotionValue) { emotionValue = emotion.Disgust; emotionType = "Disgust"; }
-                    if (emotion.Fear > emotionValue) { emotionValue = emotion.Fear; emotionType = "Fear"; }
-                    if (emotion.Happiness > emotionValue) { emotionValue = emotion.Happiness; emotionType = "Happiness"; }
-                    if (emotion.Neutral > emotionValue) { emotionValue = emotion.Neutral; emotionType = "Neutral"; }
-                    if (emotion.Sadness > emotionValue) { emotionValue = emotion.Sadness; emotionType = "Sadness"; }
-                    if (emotion.Surprise > emotionValue) { emotionType = "Surprise"; }
-                    Console.WriteLine($"Emotion : {emotionType}");
-                    Console.WriteLine();
-                    switch (emotionType)
-                    {
-                        case "Anger":
-                            anger++;
-                            break;
-                        case "Contempt":
-                            contempt++;
-                            break;
-                        case "Disgust":
-                            disgust++;
-                            break;
-                        case "Fear":
-                            fear++;
-                            break;
-                        case "Happiness":
-                            happiness++;
-                            break;
-                        case "Neutral":
-                            neutral++;
-                            break;
-                        case "Sadness":
-                            sadness++;
-                            break;
-                        case "Surprise":
-                            surprise++;
-                            break;
-                        default:
-                            break;
-                    }
-                }
+                //save the highest score of the emotion for the given image 
+                imageMoods[i] = GetReplyText(GetHeaviestEmotion(detectedFaces));
             }
+
+            Hashtable hs = new Hashtable();
+            //get the average emotion of all the images 
+            int mood = findMostOccuringElement(imageMoods, hs);
+            Console.WriteLine();
+            //print the average emotion of all the images 
             Console.WriteLine("===========================");
-            Console.WriteLine($"Anger: {anger} Contempt: {contempt} Disgust: {disgust} Fear: {fear}");
-            Console.WriteLine($"Happiness: {happiness} Neutral: {neutral} Sadness: {sadness} Surprise: {surprise}");
+            Console.WriteLine("The most occuring emotion: " + moodType(mood));
             Console.WriteLine("===========================");
+            Console.WriteLine();
         }
+
+
+            //    // Parse and print all attributes of each detected face.
+            //    foreach (var face in detectedFaces)
+            //    {
+            //        Console.WriteLine($"Face attributes for {i}.jpg:");
+            //        // Get bounding box of the faces
+            //        Console.WriteLine($"Rectangle(Left/Top/Width/Height) : " +
+            //            $"{face.FaceRectangle.Left} {face.FaceRectangle.Top} {face.FaceRectangle.Width} {face.FaceRectangle.Height}");
+            //        // Get emotion on the face
+            //        string emotionType = string.Empty;
+            //        double emotionValue = 0.0;
+            //        Emotion emotion = face.FaceAttributes.Emotion;
+            //        if (emotion.Anger > emotionValue) { emotionValue = emotion.Anger; emotionType = "Anger"; }
+            //        if (emotion.Contempt > emotionValue) { emotionValue = emotion.Contempt; emotionType = "Contempt"; }
+            //        if (emotion.Disgust > emotionValue) { emotionValue = emotion.Disgust; emotionType = "Disgust"; }
+            //        if (emotion.Fear > emotionValue) { emotionValue = emotion.Fear; emotionType = "Fear"; }
+            //        if (emotion.Happiness > emotionValue) { emotionValue = emotion.Happiness; emotionType = "Happiness"; }
+            //        if (emotion.Neutral > emotionValue) { emotionValue = emotion.Neutral; emotionType = "Neutral"; }
+            //        if (emotion.Sadness > emotionValue) { emotionValue = emotion.Sadness; emotionType = "Sadness"; }
+            //        if (emotion.Surprise > emotionValue) { emotionType = "Surprise"; }
+            //        Console.WriteLine($"Emotion : {emotionType}");
+            //        Console.WriteLine();
+            //        switch (emotionType)
+            //        {
+            //            case "Anger":
+            //                anger++;
+            //                break;
+            //            case "Contempt":
+            //                contempt++;
+            //                break;
+            //            case "Disgust":
+            //                disgust++;
+            //                break;
+            //            case "Fear":
+            //                fear++;
+            //                break;
+            //            case "Happiness":
+            //                happiness++;
+            //                break;
+            //            case "Neutral":
+            //                neutral++;
+            //                break;
+            //            case "Sadness":
+            //                sadness++;
+            //                break;
+            //            case "Surprise":
+            //                surprise++;
+            //                break;
+            //            default:
+            //                break;
+            //        }
+            //    }
+            //}
+            //Console.WriteLine("===========================");
+            //Console.WriteLine($"Anger: {anger} Contempt: {contempt} Disgust: {disgust} Fear: {fear}");
+            //Console.WriteLine($"Happiness: {happiness} Neutral: {neutral} Sadness: {sadness} Surprise: {surprise}");
+            //Console.WriteLine("===========================");
     }
 }
